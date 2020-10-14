bfef03eb736166abd021d8d18ddb3fb9869ef2b4
Dip Ghosh <dipnsucse@gmail.com>
2020-10-14 23:29:50 +0600

    final upload

---
+++ js/js.js
@@ -0,0 +1,24 @@
+/*slider js starts*/
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    dots:false,
+    nav:true,
+    mouseDrag:false,
+    autoplay:true,
+    animateOut: 'slideOutRight',
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+});
+
+/*slider js ends*/
